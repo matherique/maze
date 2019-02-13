23df0a42caee63f9887abba4634b174e3e982566
Matheus Henrique dos Santos <matherique@gmail.com>
2019-02-13 14:32:40 -0200

    first commit

---
+++ maze.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+from random import choice
+
+WIDTH, HEIGHT = (400, 400)
+w = 10
+ 
+current = None
+
+cols = WIDTH // w
+rows = HEIGHT // w
+
+grid = []
+stack = []
+
+maze = {
+    'width': WIDTH,
+    'height': HEIGHT,
+    'w': w,
+    'cols': cols,
+    'rows' : rows,
+    'data': [],
+    'current_path': []
+}  
+
+# calculate index from i and j and validate 
+def index (i, j):
+    if i < 0 or j < 0 or i > (cols - 1) or j > (rows - 1):
+        return len(grid) + 1
+
+    return i + (j * cols)
+
+
+# check if index is in list, if not return None
+def checkinlist(idx):
+    try:
+        return grid[idx]
+    except IndexError:
+        return False
+
+
+class Cell:
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+        self.visited = False
+        self.walls = [True, True, True, True]
+    
+    def checkNeighbors(self):
+        neigh = []
+        
+        top     = checkinlist(index(self.i, self.j - 1))
+        right   = checkinlist(index(self.i + 1, self.j))
+        bottom  = checkinlist(index(self.i, self.j + 1))
+        left    = checkinlist(index(self.i - 1, self.j))
+
+        if (top and not top.visited):
+            neigh.append(top)
+
+        if (right and not right.visited):
+            neigh.append(right)
+
+        if (bottom and not bottom.visited):
+            neigh.append(bottom)
+
+        if (left and not left.visited):
+            neigh.append(left)
+
+        if len(neigh) > 0:
+            return choice(neigh)
+        else:
+            return False
+
+    def __str__(self):
+        #return "i: %d j: %d" % (self.i, self.j)
+        return "index: %d" % index(self.i, self.j)
+
+    def show(self):
+        return {
+            'top': int(self.walls[0]), 
+            'right': int(self.walls[1]), 
+            'bottom': int(self.walls[2]), 
+            'left' : int(self.walls[3]) 
+        } 
+
+    def removeWall(self, cell):
+        a, b = self, cell
+        x = a.i - b.i
+        if x == 1:
+            a.walls[3] = False
+            b.walls[1] = False
+        elif x == -1:            
+            a.walls[1] = False
+            b.walls[3] = False
+               
+        y = a.j - b.j
+ 
+        if y == 1:
+            a.walls[0] = False
+            b.walls[2] = False
+        elif y == -1:            
+            a.walls[2] = False
+            b.walls[0] = False
+
+for j in range(rows):
+    for i in range(cols):
+        grid.append(Cell(i, j))
+
+current = grid[0]
+all_visit = False
+
+while not all_visit:
+    maze['current_path'].append(index(current.i, current.j))
+    current.visited = True
+    # step 1
+    nextN = current.checkNeighbors()
+    if nextN:        
+        nextN.visited = True
+        stack.append(current)
+        # step 3
+        current.removeWall(nextN)
+        # step 4
+        current = nextN
+    elif len(stack) > 0:
+      current = stack.pop()
+    visit = 0
+    for g in grid:
+        if g.visited:
+            visit += 1
+
+    print("Cells visited %d/%d" % (visit, len(grid)))
+    all_visit = (visit == len(grid))
+       
+
+for g in grid:
+    maze['data'].append(g.show())
+
+import json 
+
+filename = "data-%dx%d.json" % (WIDTH, HEIGHT)
+with open(filename, 'w') as fp:
+    json.dump(maze, fp)
+
+
+
---
+++ sketch.js
@@ -0,0 +1,89 @@
+
+let maze = {};
+let grid = [];
+let cols, rows, w, count = 0;
+
+function preload() {
+	maze = loadJSON('data-400x400.json');
+}
+
+function setup() {
+	createCanvas(maze.width, maze.height);
+	cols = maze.cols;
+	rows = maze.rows;
+	frameRate(5)
+	for (let j = 0; j < rows; j++) {
+		for (let i = 0; i < cols; i++) {
+			grid.push(new Cell(i, j));
+		}
+	}
+	maze.data;
+	w = maze.w
+}
+
+function draw() {
+	background(50);
+
+	for (let i = 0; i < grid.length; i++) {
+		grid[i].show();
+		grid[i].getWalls();
+	}
+	if (maze.current_path) {
+		if (count < maze.current_path.length) {
+			let i = maze.current_path[count];
+			grid[i].showCurrent()
+			count++;
+		}
+	}
+}
+
+function index(i, j) {
+	return i + j * cols
+}
+
+class Cell {
+	constructor(i, j) {
+		this.i = i;
+		this.j = j;
+		this.walls = [true, true, true, true];
+	}
+
+	getWalls() {
+		let idx = index(this.i, this.j);
+		this.walls[0] = !!maze.data[idx].top
+		this.walls[1] = !!maze.data[idx].right
+		this.walls[2] = !!maze.data[idx].bottom
+		this.walls[3] = !!maze.data[idx].left
+	}
+
+	show() {
+		let x = this.i * maze.w;
+		let y = this.j * maze.w;
+
+		stroke(255);
+
+		if (this.walls[0]) {
+			line(x, y, x + w, y)
+		}
+
+		if (this.walls[1]) {
+			line(x + w, y, x + w, y + w)
+		}
+
+		if (this.walls[2]) {
+			line(x + w, y + w, x, y + w)
+		}
+
+		if (this.walls[3]) {
+			line(x, y + w, x, y)
+		}
+	}
+
+	showCurrent() {
+		let x = this.i * maze.w;
+		let y = this.j * maze.w;
+		fill(255, 0, 0);
+		noStroke();
+		rect(x, y, maze.w, maze.w);
+	}
+}
